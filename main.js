42f80153309b62b5b8634742d1b1113b1266d00d
nithinhasher <93312240+nithinhasher@users.noreply.github.com>
2023-07-18 20:50:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,11 @@
-// const http = require('http');
-// const PORT = 3004;
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello, Deloitte!');
-// });
-
-// server.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3004;
-
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+const http = require('http');
+const PORT = 3004;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, Deloitte!');
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
